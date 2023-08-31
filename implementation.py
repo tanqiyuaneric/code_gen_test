e8dcd0b80b39ed61946f7b735eea50fc4fbb41f0
tanqiyuan <tanqiyuaneric@163.com>
2023-09-01 07:42:44 +0800

    update files

--- implementation.py
+++ implementation.py
@@ -2,6 +2,17 @@
 # @Time : 2023/8/31 23:36
 # @Author : Tan Qiyuan
 # @File : implementation
+import openai
+
+openai.api_key = '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'
+
 
 def generate_one_completion(prompt):
     print(prompt)
+
+
+def planning(prompt):
+    openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[{'role': 'system', 'content': ''}]
+    )
