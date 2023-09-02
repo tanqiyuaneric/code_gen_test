cba873e56636a971449c22136b2f44bb80a14950
Eric Tan <tanqiyuaneric@163.com>
2023-09-02 08:43:47 +0800

    added a progress bar; added the implementation for zero-shot code direct code generation

--- implementation.py
+++ implementation.py
@@ -3,16 +3,35 @@
 # @Author : Tan Qiyuan
 # @File : implementation
 import openai
+from human_eval.data import read_problems
 
 openai.api_key = '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'
+openai.api_base = "http://localhost:8000/v1"
 
+problems = read_problems()
 
+# ONE_SHOT_PROMPT =
 def generate_one_completion(prompt):
-    print(prompt)
+    return planning(prompt)
 
 
 def planning(prompt):
-    openai.ChatCompletion.create(
-        model='gpt-3.5-turbo',
-        messages=[{'role': 'system', 'content': ''}]
+    chat = openai.Completion.create(
+        model='chatglm2',
+        prompt=prompt
     )
+    print(f'planning: {chat.choices[0].text}')
+    return chat.choices[0].text
+
+
+def direct_generation(prompt, model):
+    chat = openai.Completion.create(
+        model=model,
+        prompt=prompt
+    )
+    # print(f'{prompt+chat.choices[0].text}')
+    return chat.choices[0].text
+
+
+if __name__ == '__main__':
+    print(planning('hello'))
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+from tqdm import tqdm  # 导入tqdm库
+
 from human_eval.data import write_jsonl, read_problems
-from implementation import generate_one_completion
+from implementation import *
 
 problems = read_problems()
+model = 'chatglm2-6b'
 
-num_samples_per_task = 1
-samples = [
-    dict(task_id=task_id, completion=generate_one_completion(problems[task_id]["prompt"]))
-    for task_id in problems
-    for _ in range(num_samples_per_task)
-]
+num_samples_per_task = 3
+
+samples = []
+
+keys = list(problems.keys())
+
+# 计算总迭代次数
+total_iterations = num_samples_per_task * len(keys)
+
+# 使用tqdm创建一个进度条
+with tqdm(total=total_iterations, desc='Generating samples') as pbar:
+    for _ in range(num_samples_per_task):
+        for task_id in keys:
+            samples.append(dict(task_id=task_id, completion=direct_generation(problems[task_id]["prompt"], model)))
+            pbar.update(1)  # 更新进度条
+
 write_jsonl("samples.jsonl", samples)
